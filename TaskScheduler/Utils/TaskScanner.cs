e3be35b49b25034883674f1f913b183f149ca752
bibletoon <alex8.ivanov@gmail.com>
2023-03-12 22:37:23 +0300

    Feat: add scanner cache

--- TaskScheduler/Utils/TaskScanner.cs
+++ TaskScheduler/Utils/TaskScanner.cs
@@ -4,18 +4,25 @@ namespace TaskScheduler.Utils;
 
 public static class TaskScanner
 {
+    private static readonly Dictionary<string, Type> Cache = new Dictionary<string, Type>();
+
     public static List<Type> GetAllTasks()
     {
         var interfaceType = typeof(IPeriodicalTask);
-        return AppDomain.CurrentDomain
+        var tasks = AppDomain.CurrentDomain
             .GetAssemblies()
             .SelectMany(a => a.GetTypes())
             .Where(t => t.IsAssignableTo(interfaceType))
             .ToList();
+        
+        tasks.ForEach(t => Cache[t.Name] = t);
+        return tasks;
     }
 
     public static Type? GetTask(string name)
     {
-        return GetAllTasks().FirstOrDefault(t => t.Name == name);
+        return Cache.ContainsKey(name) 
+            ? Cache[name] 
+            : GetAllTasks().FirstOrDefault(t => t.Name == name);
     }
 }
